b9579037fa13feec93db2f54fa24b0fa348c9d8f
Alison <zhangyinag@126.com>
2019-06-05 21:13:55 +0800

    1.0.4

--- src/index.js
+++ src/index.js
@@ -1,6 +1,11 @@
 require('babel-register')({
   presets: [ 'env' ],
-  plugins: ['transform-decorators-legacy']
+  plugins: ['transform-decorators-legacy'],
+  ignore: function (filename) {
+    if (!filename.includes('node_modules')) return false
+    if (filename.includes('node_modules/@pat-fet/mock-server')) return true
+    return true
+  }
 })
 
 module.exports = require('./MockServer').default
